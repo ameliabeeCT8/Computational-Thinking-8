0e177e486f22cdf28bd03a9e9166b5bffa339db9
ameliabeeCT8 <ameliabeaumont@seattleacademy.org>
2026-01-20 16:58:58 +0000

    p3

--- Projects/project3.py
+++ Projects/project3.py
@@ -2,19 +2,17 @@ import turtle, time, random
 from utils import *
 
 # Section 1 - Variables
-# TODO - add starting values for all the variables
 x1 = -200
 y1 = -90
 x2 = 100
-y2 = -90
+y2 = 0
 x3 = 0
-y3 = -90
+y3 = 50
 x4 = -100
-y4 = -90
+y4 = 150
 
 
 # Section 2 - Setup
-# # TODO - use your own background, and set your four turtles to images of your choice
 set_background("moon")
 t1 = create_sprite("kitten",x1,y1)
 t2 = create_sprite("alien",x2,y2)
@@ -22,12 +20,12 @@ t3 = create_sprite("fish",x3,y3)
 t4 = create_sprite("fox",x4,y4)
 
 
-# # Section 3 - Racing
-# # TODO - set how much each variable changes by and increase the number of repeats to at least 30
-# # TODO - explain here which sprites are faster or slower
-for i in range(3):
-    x1 += 15
-    x2 += 10
+# Section 3 - Racing
+# The alien is the fastest sprite because its point goes += 15 and for all the other sprites
+# the += is a lower number.
+for i in range(30):
+    x1 += 10
+    x2 += 15
     x3 += 5
     x4 += 8
 
@@ -40,13 +38,15 @@ for i in range(3):
     time.sleep(0.1)
 
 
-# # Section 4 - Winner
-# # TODO - complete the elif for player 2 winning
-# # TODO - write another elif for player 3 and player 4
-# if x1 >= x2 and x1 >= x3 and x1 >= x4:
-#     print("player 1 wins!")
-# elif
-#     print("player 2 wins!")
+# Section 4 - Winner
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+    print("kitten wins!")
+elif x2 >= x1 and x2 >= x3 and x2 >= x4:
+    print("alien wins!")
+elif x3 >= x1 and x3 >= x2 and x3 >= x4:
+    print("fish wins!")
+elif x4 >= x1 and x4 >= x2 and x4 >= x3:
+    print("fox wins!")
 
 
 turtle.exitonclick()
